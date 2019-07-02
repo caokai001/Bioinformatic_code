89572831c3ffc29b8b811550baa9da41b81aebe0
kcao <33218753+caokai001@users.noreply.github.com>
2019-07-02 14:08:40 +0800

    Update and rename BH.py to FDR.py

--- python/生信技能树python练习题/FDR.py
+++ python/生信技能树python练习题/FDR.py
@@ -1,15 +1,23 @@
+## 对p值校正
+
 import numpy as np
 import pandas as pd
-
+##使用pd.Dataframe 更加方便
 
 def p_adjust_bh(p):
     """BH"""
     a = ["G"] * len(p)
     b = np.arange(1, len(p)+1)
     c = [str(i) + str(j) for i, j in zip(a, b)]
-    P = pd.(p, index=c)
+    P = pd.Series(p, index=c)
 
     stat = np.arange(1, len(P) + 1) * 0.05 / len(P)
     return [P.values.tolist().index(i)+1 for i, j in zip(P[P.argsort().values], stat) if i<j]
 
 p_adjust_bh(p)
+
+################
+#p
+#Out[131]: [0.053, 0.001, 0.045, 0.03, 0.02, 0.01]
+#p_adjust_bh(p)
+#Out[132]: [2, 6, 5, 4]
